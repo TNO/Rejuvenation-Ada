0921634b391c1820319b457b52b709adffd7bb46
Fabien Chouteau <chouteau@adacore.com>
2022-04-11 17:11:18 +0200

    workshop/tests/src/test_workshop.adb: exit with error on test failure

--- workshop/tests/src/test_workshop.adb
+++ workshop/tests/src/test_workshop.adb
@@ -2,9 +2,15 @@ with AUnit.Reporter.Text;
 with AUnit.Run;
 with Workshop_Suite; use Workshop_Suite;
 
+with GNAT.OS_Lib;
+
 procedure Test_Workshop is
-   procedure Runner is new AUnit.Run.Test_Runner (Suite);
+   use type AUnit.Status;
+
+   function Runner is new AUnit.Run.Test_Runner_With_Status (Suite);
    Reporter : AUnit.Reporter.Text.Text_Reporter;
 begin
-   Runner (Reporter);
+   if Runner (Reporter) /= AUnit.Success then
+      GNAT.OS_Lib.OS_Exit (1);
+   end if;
 end Test_Workshop;
