938fd001aeadd3332f2254cced44afa53ae38158
Pierre van de Laar <pierre.van.de.laar@nexperia.com>
2022-04-12 16:38:56 +0200

    more consistent naming

--- workshop/tests/src/tests_workshop.adb
+++ workshop/tests/src/tests_workshop.adb
@@ -4,11 +4,11 @@ with AUnit.Run;           use AUnit.Run;
 with GNAT.OS_Lib;         use GNAT.OS_Lib;
 with Workshop_Suite;      use Workshop_Suite;
 
-procedure Test_Workshop is
+procedure Tests_Workshop is
    function Runner is new Test_Runner_With_Status (Suite);
    Reporter : Text_Reporter;
 begin
    if Runner (Reporter) /= Success then
       OS_Exit (1);
    end if;
-end Test_Workshop;
+end Tests_Workshop;
