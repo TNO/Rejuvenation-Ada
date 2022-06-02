1d38bae7feeb30c93457e3df31549ff0b8961d93
pjljvandelaar <pierre.vandelaar@tno.nl>
2022-06-02 14:15:50 +0200

    Functionality to set multiple environment variables at once

---
+++ src/rejuvenation-environment_variables.adb
@@ -0,0 +1,20 @@
+with Ada.Environment_Variables; use Ada.Environment_Variables;
+
+package body Rejuvenation.Environment_Variables is
+
+   procedure Set (Map : String_Maps.Map) is
+
+      procedure Set_Position (Position : Cursor);
+      --  Set individual key value pair at position
+      procedure Set_Position (Position : Cursor)
+      is
+      begin
+         Set (Name => Key (Position),
+              Value => Element (Position));
+      end Set_Position;
+
+   begin
+      Map.Iterate (Set_Position'Access);
+   end Set;
+
+end Rejuvenation.Environment_Variables;
---
+++ src/rejuvenation-environment_variables.ads
@@ -0,0 +1,9 @@
+with String_Maps;  use String_Maps;
+
+package Rejuvenation.Environment_Variables is
+
+   procedure Set (Map : String_Maps.Map);
+   --  Set all environment variables
+   --  according to the key value pairs in the map
+
+end Rejuvenation.Environment_Variables;
