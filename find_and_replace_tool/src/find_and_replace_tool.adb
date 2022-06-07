4cde0efc4d3ca63b77c08f2d610071f9c35b38c2
pjljvandelaar <pierre.vandelaar@tno.nl>
2022-06-07 17:26:13 +0200

    Follow changes in rejuvenation interface

--- find_and_replace_tool/src/find_and_replace_tool.adb
+++ find_and_replace_tool/src/find_and_replace_tool.adb
@@ -1,10 +1,10 @@
-with Ada.Strings.Unbounded;          use Ada.Strings.Unbounded;
 with Ada.Text_IO;                    use Ada.Text_IO;
 with Libadalang.Common;              use Libadalang.Common;
 with Rejuvenation;                   use Rejuvenation;
 with Rejuvenation.Find_And_Replacer; use Rejuvenation.Find_And_Replacer;
 with Rejuvenation.Patterns;          use Rejuvenation.Patterns;
 with Rejuvenation.Simple_Factory;    use Rejuvenation.Simple_Factory;
+with String_Vectors;                 use String_Vectors;
 
 procedure Find_And_Replace_Tool is
    Find_Pattern : constant Pattern :=
@@ -15,16 +15,12 @@ procedure Find_And_Replace_Tool is
      Make_Pattern ("$S_F ($S_Arg2, $S_Arg1);", Call_Stmt_Rule);
 
    Project_Name : constant String := "C:\path\to\your.gpr";
-   UFiles : constant Unbounded_Strings.Vector :=
+   Files : constant String_Vectors.Vector :=
      Get_Ada_Source_Files_From_Project (Project_Name);
 begin
-   for UFile of UFiles loop
-      declare
-         File  : constant String  := To_String (UFile);
-      begin
-         if Find_And_Replace (File, Find_Pattern, Replace_Pattern) then
-            Put_Line ("Changed - " & File);
-         end if;
-      end;
+   for File of Files loop
+      if Find_And_Replace (File, Find_Pattern, Replace_Pattern) then
+         Put_Line ("Changed - " & File);
+      end if;
    end loop;
 end Find_And_Replace_Tool;
